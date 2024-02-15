bcf5834e2b1195556afe6375cc57c995a21d7367
mskhan1995 <shabhankhan123456@gmail.com>
2024-02-15 21:20:16 +0530

    Home and about paragraph change

--- src/components/about/Info.jsx
+++ src/components/about/Info.jsx
@@ -1,27 +1,28 @@
 import React from "react";
+import "./about.css";
 
-const Info = ()=>{
-    return(
-        <div className="about__info grid">
-            <div className="about__box">
-                <i class='bx bx-award about__icon ' ></i>
-                <h3 className="about__title">Communicator</h3>
-                <span className="about__subtitle">Good communicator</span>
-            </div>
+const Info = () => {
+  return (
+    <div className="about__info grid">
+      <div className="about__box">
+        <i class="bx bx-award about__icon "></i>
+        <h3 className="about__title">Communicator</h3>
+        <span className="about__subtitle">Good communicator</span>
+      </div>
 
-            <div className="about__box">
-                <i class='bx bx-briefcase-alt about__icon' ></i>
-                <h3 className="about__title">Completed</h3>
-                <span className="about__subtitle">15+ projects</span>
-            </div>
+      <div className="about__box">
+        <i class="bx bx-briefcase-alt about__icon"></i>
+        <h3 className="about__title">Completed</h3>
+        <span className="about__subtitle">15+ projects</span>
+      </div>
 
-            <div className="about__box">
-                <i class="bx bx-brain about__icon"></i>
-                <h3 className="about__title">Solved</h3>
-                <span className="about__subtitle">400+ DSA questions</span>
-            </div>
-        </div>
-    )
-}
+      <div className="about__box">
+        <i class="bx bx-brain about__icon"></i>
+        <h3 className="about__title">Solved</h3>
+        <span className="about__subtitle">400+ DSA questions</span>
+      </div>
+    </div>
+  );
+};
 
 export default Info;
